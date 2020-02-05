b39720a492083b6ee48b774f3d5d7490596e8398
junsooyeo <59544981+junsooyeo@users.noreply.github.com>
2020-02-06 00:28:15 +0900

    Day 2-3 - 2020.02.05
    adjust variables

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from indeed import get_indeed_jobs
 from saramin import get_saramin_jobs
-#indeed_jobs = get_indeed_jobs()
+from save import save_to_file
 
+indeed_jobs = get_indeed_jobs()
 saramin_jobs = get_saramin_jobs()
+jobs = indeed_jobs+saramin_jobs
+save_to_file(jobs)
 
-print(saramin_jobs)
---
+++ save.py
@@ -0,0 +1,12 @@
+import csv
+#-*- coding:utf-8 -*-
+
+
+def save_to_file(jobs):
+    file = open("jobs.csv",mode="w",encoding='utf8')
+    writer = csv.writer(file)
+    writer.writerow(["title", "company", "location", "link"])
+
+    for job in jobs:
+        writer.writerow(list(job.values()))
+    return
